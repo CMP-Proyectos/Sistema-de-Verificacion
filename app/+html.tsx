dc0ff66b31f79db78458826dd93621cf54f05c21
Juan Diego Huaylupo <jdhuaylupo@gmail.com>
2026-04-14 12:18:39 -0500

    ajustar dimensiones de iconos web

--- app/+html.tsx
+++ app/+html.tsx
@@ -11,9 +11,9 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="google-site-verification" content="0cyCWEMy_FexMpfBARoI48z4Bddzzy6dkRsg0blmWD8" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/iconos/escudo-dim.png" />
+        <link rel="icon" href="/iconos/escudo-dim2.png" />
 
-        <link rel="apple-touch-icon" href="/iconos/escudo-dim.png" />
+        <link rel="apple-touch-icon" href="/iconos/escudo-dim2.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="SIVEO" />
