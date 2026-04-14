49e73311d983f25fdaa216ec5094022f540bb592
Juan Diego Huaylupo <jdhuaylupo@gmail.com>
2026-04-14 13:16:13 -0500

    actualizar iconos y repomix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "reportflow-v3";
+const CACHE_VERSION = "reportflow-v5";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const OFFLINE_HTML_URL = "/index.html";
